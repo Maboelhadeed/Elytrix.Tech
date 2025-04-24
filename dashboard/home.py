3e8679053a7fad412d01d108050f99e706608605
MAboelhadeed <moustafa.aboelhadeed92@gmail.com>
2025-04-24 19:41:26 +0100

    Update home.py

--- dashboard/home.py
+++ dashboard/home.py
@@ -1,33 +1,14 @@
 import streamlit as st
+import os
 
 def render_home():
-    theme = st.session_state.get('theme', 'Dark')
-    
-    # Background style
-    if theme == "Dark":
-        st.markdown("""
-            <style>
-                .stApp {
-                    background-color: #0e1117;
-                    color: white;
-                }
-            </style>
-        """, unsafe_allow_html=True)
-    else:
-        st.markdown("""
-            <style>
-                .stApp {
-                    background-color: #ffffff;
-                    color: black;
-                }
-            </style>
-        """, unsafe_allow_html=True)
+    # Set Elytrix logo path
+    logo_path = os.path.join("dashboard", "assets", "elytrix_logo.png")
 
-    # Centered content
-    st.markdown("<br><br>", unsafe_allow_html=True)
-    st.markdown("<div style='text-align:center'>"
-                "<img src='https://raw.githubusercontent.com/Maboelhadeed/Elytrix.tech/main/dashboard/assets/elytrix_logo.png' width='160'>"
-                "<h1>Welcome to Elytrix</h1>"
-                "<p><i>Precision Wins.</i></p>"
-                "<a href='/?Navigate=Dashboard'><button style='padding:10px 20px; font-size:16px; background-color:#292929; color:white; border:none; border-radius:5px;'>Launch Dashboard</button></a>"
-                "</div>", unsafe_allow_html=True)
+    st.image(logo_path, width=180)
+    st.markdown("<h1 style='text-align: center;'>Welcome to Elytrix</h1>", unsafe_allow_html=True)
+    st.markdown("<h4 style='text-align: center; font-style: italic;'>Precision Wins.</h4>", unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    if st.button("Launch Dashboard", use_container_width=True):
+        st.switch_page("pages/dashboard_ui.py")
